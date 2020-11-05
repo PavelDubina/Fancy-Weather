989ab6c72a0dbc31dfe74c1e4e77dc9f73dcd6e4
Pavel Dubina <Pavel_Dubina@epam.com>
2020-11-05 18:06:20 +0300

    feat: continue my work)

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const background = document.querySelector('.app--container');
 const refreshImg = document.querySelector('.refresh--img');
 
 const getBackgroundImage = async () => {
-  try{
+  try {
     const result = await fetch(`https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=nature&client_id=Kb3VAsmyDsoRtVFNxYGDVeEHvoY1QLdStWEs1xe5MKU`)
     const data = await result.json();
     return data?.urls?.regular;
-  } catch(e) {
+  } catch (e) {
     console.error(e)
     background.style.backgroundImage = "url('./img/background.jpg')"
   }
-  
+
 }
 
 const refreshBackgroundImage = () => {
@@ -22,31 +22,114 @@ const refreshBackgroundImage = () => {
 
 refreshImgBtn.addEventListener('click', refreshBackgroundImage)
 refreshImg.addEventListener('animationend', () => refreshImg.classList.remove('active--refresh'))
-// /////////////////refresh picture///////////////
+///////////////////refresh picture///////////////
 
 
 
+
+
+
+
+
+
+
+////////map///////////
+const searchPlace = document.querySelector('.input--block')
+const searchBtn = document.querySelector('.search--btn')
+const cityAndCountry = document.querySelector('.location--text')
+const latitude = document.querySelector('.latitude')
+const longitude = document.querySelector('.longitude')
+let searchPlaceInFocus = false;
+
+
+/////////////Find my coordinates//////////////////////////
 const findGeolocation = async () => {
   const result = await fetch('https://ipinfo.io/json?token=889c7e574eacbf')
   const data = await result.json();
-  console.log(data)
+  let coordinates = data.loc.split(',').map((i) => {
+    return +i
+  })
+  return coordinates
 }
 
-findGeolocation()
-navigator.geolocation.getCurrentPosition(position => {
-  console.log(position)
- })
-
-//  const findCountry = async () => {
-//   const result = await fetch('https://ipinfo.io/json?token=889c7e574eacbf')
-//   const data = await result.json();
-//   console.log(data)
-// }
-
-const findMap = async () => {
-  const result = await fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/23.737139199999998,52.0975.json?access_token=pk.eyJ1IjoicGF2ZWxla25hbWUiLCJhIjoiY2toM2llY2w3MGV1MTJ5bHNlcjhzMTFuaCJ9.Gvc2M_yIZPhIOWwgG5t4BA")
-  const data = await result.json();
-  console.log(data)
+const createMap = (container, coordinats, zoom) => {
+  return new ymaps.Map(container, {
+    center: coordinats, // Your location
+    zoom: zoom,
+    controls: ['smallMapDefaultSet'],
+    type: 'yandex#hybrid'
+  }, {
+    searchControlProvider: 'yandex#search'
+  });
 }
 
-findMap()
+const installMyGeolocation = (myMap) => {
+  findGeolocation().then((location) => {
+  changeMap(location, myMap)
+  })
+}
+
+const findLocation = (myMap) => {
+  const locationValue = searchPlace.value;
+  if(locationValue === '') return
+  changeMap(locationValue, myMap);
+  searchPlace.value = ''
+}
+
+const changeMap = (locationValue, myMap) => {
+  return ymaps.geocode(locationValue)
+    .then((result) => { 
+      try{
+        const location = result.geoObjects.get(0)
+        const country = location.getCountry()
+        const city = location.getLocalities().length > 1 ? location.getLocalities()[1] : location.getLocalities()[0]
+        const coordinates = typeof locationValue === 'string' ? location.geometry.getCoordinates() : locationValue
+        let [latitudeValue, longitudeValue] = coordinates;
+        latitude.innerHTML = `Широта: ${String(latitudeValue.toFixed(2)).replace(/[.]/g, '°')}'`;
+        longitude.innerHTML = `Долгота: ${String(longitudeValue.toFixed(2)).replace(/[.]/g, '°')}'`;
+        cityAndCountry.innerHTML = `${city?city:''} ${country}`
+        myMap.panTo(coordinates)
+      } catch(error){
+        searchPlace.value = 'Ошибка запроса. Повторите пожалуйста.'
+      } 
+    })
+}
+
+const init = () => {
+  const myMap = createMap('map', [45, 20], 12)
+  installMyGeolocation(myMap);
+  searchBtn.addEventListener('click', findLocation.bind(null, myMap))
+}
+
+searchPlace.addEventListener('focus', () => {
+  searchPlace.value = '';
+  searchPlaceInFocus = !searchPlaceInFocus;
+})
+searchPlace.addEventListener('blur', () => {
+  searchPlaceInFocus = !searchPlaceInFocus;
+})
+
+window.addEventListener('keydown', (e) => {
+  if(e.key === 'Enter' && searchPlaceInFocus){
+    searchBtn.click()
+  }
+})
+
+ymaps.ready(init);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
