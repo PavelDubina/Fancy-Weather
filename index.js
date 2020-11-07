5f1fd47febf4435bacc5cf1f79866b8068ee552e
Pavel Dubina <pavelek_name@mail.ru>
2020-11-07 19:03:59 +0300

    .

--- index.js
+++ index.js
@@ -26,13 +26,6 @@ refreshImg.addEventListener('animationend', () => refreshImg.classList.remove('a
 
 
 
-
-
-
-
-
-
-
 ////////map///////////
 const searchPlace = document.querySelector('.input--block')
 const searchBtn = document.querySelector('.search--btn')
@@ -40,10 +33,19 @@ const cityAndCountry = document.querySelector('.location--text')
 const latitude = document.querySelector('.latitude')
 const longitude = document.querySelector('.longitude')
 const datePlace = document.querySelector('.date--text')
+const currentWeatherTemp = document.querySelector('.weather--temp')
+const descriptionWeatherText = document.querySelector('.description')
+const feelsLikeText = document.querySelector('.feels')
+const windText = document.querySelector('.wind')
+const humidityText = document.querySelector('.humidity')
+const currentWeatherImg = document.querySelector('.weather--temp--img img')
+const forecastDayText = document.querySelectorAll('.future--day--name')
+const forecastTempText = document.querySelectorAll('.future--temp')
+const forecastImg = document.querySelectorAll('.future--img')
 let searchPlaceInFocus = false;
 let interval;
 
-
+console.log(currentWeatherImg.src)
 
 /////////////Find my coordinates//////////////////////////
 const findGeolocation = async () => {
@@ -74,6 +76,8 @@ const findNewLocation = (map) => {
   updateAppData(locationValue).then(coordinates => map.panTo(coordinates))
   updateTime(locationValue)
   searchPlace.value = ''
+  updateCurrentWeather(locationValue)
+  updateForecastWeather(locationValue)
 }
 
 ///////////////////Получение объекта данных из карты///////////////
@@ -88,11 +92,7 @@ const getData = async (coords) => {
   const country = geoObject.getCountry()
   const city = geoObject.getLocalities().length > 1 ? geoObject.getLocalities()[1] : geoObject.getLocalities()[0]
   const coordinates = typeof locationValue === 'string' ? geoObject.geometry.getCoordinates() : locationValue
-  return placeData = {
-    country,
-    city,
-    coordinates
-  }
+  return {country, city, coordinates}
 }
 //////////////// Обновление данных////////////////////
 const updateAppData = async (coords) => {
@@ -129,7 +129,6 @@ const getTime = (timeZone) =>{
 }
 //////////////////////////////////////////////////////////////
 
-
 ////////////////////////Находим временную зону////////////////////////////////
 const getTimeZone = async (latitude, longitude) => {
   const result = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=c127895a61b649119859150903a3c8db&pretty=1`)
@@ -137,24 +136,78 @@ const getTimeZone = async (latitude, longitude) => {
   const timeZone = data?.results[0]?.annotations?.timezone?.name;
   return timeZone
 }
-
-
-
-const getWeather = async () => {
-  const result = await fetch(`https://api.climacell.co/v3/weather/forecast/daily?lat=55.7522&lon=37.6156&unit_system=si&start_time=now&fields=feels_like%temp%humidity%wind_speed%weather_code&apikey=wec7hC5mQw2LqED55M0I8vdGHH0JoNAO`)
+//////////////////Получаем ответ на запрос о погоде/////////////
+const getWeather = async (latitude, longitude) => {
+  const result = await fetch(`https://api.openweathermap.org/data/2.5/onecall?units=metric&exclude=minutely,hourly,alerts&lang=ru&lat=${latitude}&lon=${longitude}8&appid=7cf40857722b732df763dfd8436e2835`)
   const data = await result.json();
-  console.log(data)
+  return data
 }
-getWeather()
+/////////////////Получаем объект погоды///////////////////
+const getWeatherData = async (coords) => {
+  const placeData = await getData(coords);
+  const coordinates = placeData.coordinates;
+  const weatherDataObject = getWeather(...coordinates)
+  return weatherDataObject
+}
 
+const getForecastWeather = async (coords) => {
+  const weatherDataObject = await getWeatherData(coords)
+  const forecastWeatherData = weatherDataObject.daily;
+  const firstDayData = forecastWeatherData[1];
+  const secondDayData = forecastWeatherData[2];
+  const thirdDayData = forecastWeatherData[3];
+  return {firstDayData, secondDayData, thirdDayData}
+}
 
-// wec7hC5mQw2LqED55M0I8vdGHH0JoNAO
+const updateForecastWeather = async (coords) => {
+  let dayNameIndex = 0;
+  let dayTempIndex = 0;
+  const weather = await getForecastWeather(coords)
+  const arrayDayDate = [weather.firstDayData.dt * 1000, weather.secondDayData.dt * 1000, weather.thirdDayData.dt * 1000]
+  const arrayDayTemp = [weather.firstDayData.temp.day.toFixed(), weather.secondDayData.temp.day.toFixed(), weather.thirdDayData.temp.day.toFixed()]
+  forecastTempText.forEach((tempText) => {
+    tempText.innerHTML = `${arrayDayTemp[dayTempIndex]}°`
+    dayTempIndex++
+  })
+    forecastDayText.forEach((dayText) => {
+      dayText.innerHTML = (new Date(arrayDayDate[dayNameIndex])).toLocaleString('ru', {weekday: 'long'})
+      dayNameIndex++
+  }) 
 
+}
+
+///////////////Получаем и обрабатываем данные о текущей погоде//////////////////////
+const getCurrentWeather = async (coords) => {
+  const weatherDataObject = await getWeatherData(coords)
+  const currentWeatherData = weatherDataObject.current
+  const description = currentWeatherData.weather[0].description
+  const temp = currentWeatherData.temp.toFixed(0)
+  const feelsLike = currentWeatherData.feels_like.toFixed(0)
+  const wind = currentWeatherData.wind_speed
+  const humidity = currentWeatherData.humidity
+  const icon = currentWeatherData.weather[0].icon
+  return {description, temp, feelsLike, wind, humidity, icon}
+}
+/////////////////Обновляем данные о текущей погоде///////////////
+const updateCurrentWeather = async (coords) => {
+  const weather = await getCurrentWeather(coords)
+  humidityText.innerHTML = `влажность: ${weather.humidity}%`
+  windText.innerHTML = `ветер: ${weather.wind} м/с`
+  descriptionWeatherText.innerHTML = weather.description
+  currentWeatherTemp.innerHTML = weather.temp < 0 ? `${weather.temp}°` : weather.temp > 0 ? `+${weather.temp}°` : 0;
+  feelsLikeText.innerHTML = `ощущается как: ${weather.feelsLike}°`;
+  currentWeatherImg.src = `http://openweathermap.org/img/wn/${weather.icon}@4x.png`
+}
+
+///////////////////
 const init = () => {
   myMap = createMap('map', [45, 20], 12)
   updateAppData().then(coordinates => {
     myMap.panTo(coordinates)
     updateTime(coordinates)
+    updateCurrentWeather(coordinates)
+    updateForecastWeather(coordinates)
+    getWeatherData(coordinates).then((data)=>{console.log(data.daily); console.log((new Date(1604829600000)).toLocaleString('ru', {weekday: 'long'}))})
   })
   searchBtn.addEventListener('click', findNewLocation.bind(null, myMap))
 }
