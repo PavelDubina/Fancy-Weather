a562135f7f63b8305b5678153f2cd00433997c4f
Pavel Dubina <Pavel_Dubina@epam.com>
2020-11-09 17:56:00 +0300

    .

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ const getBackgroundImage = async () => {
     return data?.urls?.regular;
   } catch (e) {
     console.error(e)
-    background.style.backgroundImage = "url('./img/background.jpg')"
+    return './img/background.jpg'
   }
-
 }
 
 const refreshBackgroundImage = () => {
@@ -23,7 +22,6 @@ const refreshBackgroundImage = () => {
 refreshImgBtn.addEventListener('click', refreshBackgroundImage)
 refreshImg.addEventListener('animationend', () => refreshImg.classList.remove('active--refresh'))
 ///////////////////refresh picture///////////////
-
 
 
 ////////map///////////
@@ -40,23 +38,36 @@ const feelsLikeText = document.querySelector('.feels--text')
 const windText = document.querySelector('.wind')
 const humidityText = document.querySelector('.humidity')
 const currentWeatherImg = document.querySelector('.weather--temp--img img')
+const forecastWeatherImg = document.querySelectorAll('.future--img img')
 const forecastDayText = document.querySelectorAll('.future--day--name')
 const forecastTempText = document.querySelectorAll('.future--temp')
 const forecastImg = document.querySelectorAll('.future--img')
 const cBtn = document.querySelector('.c--temperature')
 const fBtn = document.querySelector('.f--temperature')
-const getForengeitScale = (celsiusTemp) => (celsiusTemp * 9/5) + 32
-const getCelsiusScale = (forengeitTemp) => (forengeitTemp - 32) * 5/9
+const getForengeitScale = (celsiusTemp) => (celsiusTemp * 9 / 5) + 32
+const getCelsiusScale = (forengeitTemp) => (forengeitTemp - 32) * 5 / 9
+const microphone = document.querySelector('.voice--button')
 let searchPlaceInFocus = false;
-let interval; 
+let interval;
 let cBtnActive = true
 let fBtnActive = true
+let momentCoordinates;
 
+///////////////////Создаём карту/////////////////////////
+const createMap = (container, coordinats, zoom, ymaps) => {
+  return new ymaps.Map(container, {
+    center: coordinats, // Your location
+    zoom: zoom,
+    controls: ['smallMapDefaultSet'],
+    type: 'yandex#hybrid'
+  });
+}
 
 let language;
 let myMap;
 const init = (ymaps) => {
-  myMap = createMap('map', [45, 10], 12, ymaps)
+  myMap = createMap('map', [1, 0], 12, ymaps)
+  myMap.controls.remove('smallMapDefaultSet')
   updateAppData().then(coordinates => {
     myMap.panTo(coordinates)
     updateTime(coordinates)
@@ -68,36 +79,28 @@ const init = (ymaps) => {
 }
 
 window.onload = () => {
-    // Получим ссылки на элементы с тегом 'head' и id 'language'.
-    const head = document.getElementsByTagName('head')[0];
-    const select = document.getElementById('language');
-    select.createMap = function () {
-        // Получим значение выбранного языка.
-        language = this.value;
-        // Если карта уже была создана, то удалим её.
-        if (myMap) myMap.destroy();
-        // Создадим элемент 'script'.
-        const script = document.createElement('script');
-        // Запишем ссылку на JS API Яндекс.Карт с выбранным языком в атрибут 'src'.
-        script.src = 'https://api-maps.yandex.ru/2.1/?apikey=20054a94-ac2a-468d-87b5-27c40b85b89e&onload=init_' + language + '&lang=' + language +
-            '_RU&ns=ymaps_' + language;
-        // Добавим элемент 'script' на страницу.
-        head.appendChild(script);
-        // Использование пространства имен позволяет избежать пересечения названий функций
-        // и прочих программных компонентов.
-        window['init_' + language] = () => {
-            init(window['ymaps_' + language]);
-        }
-    };
-    // Назначим обработчик для события выбора языка из списка.
-    select.addEventListener("change", select.createMap);
-    // Создадим карту и зададим для нее язык, который был выбран по умолчанию.
-    select.createMap();
+  const head = document.querySelectorAll('head')[0];
+  const select = document.querySelector('.language--select');
+  select.value = localStorage.getItem('userLang') ? localStorage.getItem('userLang') : 'en'
+  select.createMap = function () {
+    language = this.value;
+    localStorage.setItem('userLang', language)
+    if (myMap) myMap.destroy();
+    const script = document.createElement('script');
+    script.src = `https://api-maps.yandex.ru/2.1/?apikey=20054a94-ac2a-468d-87b5-27c40b85b89e&onload=init_${language}&lang=${language}_RU&ns=ymaps_${language}`;
+    head.appendChild(script);
+    window[`init_${language}`] = () => {
+      init(window[`ymaps_${language}`]);
+    }
+    refreshImgBtn.click()
+  };
+  select.addEventListener("change", select.createMap);
+  select.createMap();
 };
 
 ////////Единицы измерения по умолчанию//////////////////
 const defaultOptions = () => {
-  if(localStorage.getItem('userTempScale') === 'F'){
+  if (localStorage.getItem('userTempScale') === 'F') {
     fBtn.classList.add('active--temp')
     fBtnActive = false
   } else {
@@ -105,35 +108,15 @@ const defaultOptions = () => {
     cBtnActive = false
   }
 }
-
-
-fBtn.addEventListener('click', ()=> {
-  if(!fBtnActive) return
-  fBtn.classList.add('active--temp')
-  cBtn.classList.remove('active--temp')
-  localStorage.setItem('userTempScale', 'F')
-  currentWeatherTemp.textContent = `${Math.round(getForengeitScale(parseInt(currentWeatherTemp.textContent)))}°`
-  for(let tempText of forecastTempText){
-    tempText.textContent = `${Math.round(getForengeitScale(parseInt(tempText.textContent)))}°`
+///////////Обновление единиц измерения температуры///////////////////
+const updateScale = (foo) => {
+  currentWeatherTemp.textContent = `${Math.round(foo(parseInt(currentWeatherTemp.textContent)))}°`
+  for (let tempText of forecastTempText) {
+    tempText.textContent = `${Math.round(foo(parseInt(tempText.textContent)))}°`
   }
-  feelsLikeTemp.textContent = `${Math.round(getForengeitScale(parseInt(feelsLikeTemp.textContent)))}°`
-  fBtnActive = false
-  cBtnActive = true
-})
+  feelsLikeTemp.textContent = `${Math.round(foo(parseInt(feelsLikeTemp.textContent)))}°`
+}
 
-cBtn.addEventListener('click', ()=>{
-  if(!cBtnActive) return
-  cBtn.classList.add('active--temp')
-  fBtn.classList.remove('active--temp')
-  localStorage.setItem('userTempScale', 'C')
-  currentWeatherTemp.textContent = `${Math.round(getCelsiusScale(parseInt(currentWeatherTemp.textContent)))}°`
-  for(let tempText of forecastTempText){
-    tempText.textContent = `${Math.round(getCelsiusScale(parseInt(tempText.textContent)))}°`
-  }
-  feelsLikeTemp.textContent = `${Math.round(getCelsiusScale(parseInt(feelsLikeTemp.textContent)))}°`
-  cBtnActive = false
-  fBtnActive = true
-})
 
 /////////////Find my coordinates//////////////////////////
 const findGeolocation = async () => {
@@ -142,62 +125,63 @@ const findGeolocation = async () => {
   let coordinates = data.loc.split(',').map((i) => {
     return +i
   })
-   return coordinates
-}
-///////////////////Создаём карту/////////////////////////
-const createMap = (container, coordinats, zoom, ymaps) => {
-  return new ymaps.Map(container, {
-    center: coordinats, // Your location
-    zoom: zoom,
-    controls: ['smallMapDefaultSet'],
-    type: 'yandex#hybrid'
-  }, {
-    searchControlProvider: 'yandex#search'
-  });
+  coordinates = momentCoordinates ? momentCoordinates : coordinates;
+  return coordinates
 }
 
 /////////////////////Ищем новую локацию///////////////////////
-const findNewLocation = (map) => {
+const findNewLocation = () => {
   const locationValue = searchPlace.value;
-  if(locationValue === '') return
-  updateAppData(locationValue).then(coordinates => map.panTo(coordinates))
+  if (locationValue === '') return
+  updateAppData(locationValue).then(coordinates => {
+    momentCoordinates = coordinates;
+    myMap.panTo(coordinates, {
+      duration: 2000
+    })
+  })
   updateTime(locationValue)
   searchPlace.value = ''
   updateCurrentWeather(locationValue)
   updateForecastWeather(locationValue)
+  refreshImgBtn.click()
 }
 
 ///////////////////Получение объекта данных из карты///////////////
 const getYandexMethods = (locationValue) => {
-    return window[`ymaps_${language}`].geocode(locationValue)
+  return window[`ymaps_${language}`].geocode(locationValue)
 }
 ///////////////////Получение данных из карты//////////////////////////
 const getData = async (coords) => {
-  try{
+  try {
     const locationValue = coords ? coords : await findGeolocation()
     const yandexData = await getYandexMethods(locationValue)
     const geoObject = yandexData.geoObjects.get(0)
     const country = geoObject.getCountry()
     const city = geoObject.getLocalities().length > 1 ? geoObject.getLocalities()[1] : geoObject.getLocalities()[0]
     const coordinates = typeof locationValue === 'string' ? geoObject.geometry.getCoordinates() : locationValue
-    return {country, city, coordinates}
-  } catch(error){
+    return {
+      country,
+      city,
+      coordinates
+    }
+  } catch (error) {
     console.error(error)
-    searchPlace.value = ' The request failed'
+    searchPlace.value = language === 'en' ? 'The request failed. Repeat please' : 'Ошибка запроса. Повторите пожалуйста'
+    searchPlace.classList.add('incorrect')
     return
   }
-  
+
 }
 //////////////// Обновление данных////////////////////
 const updateAppData = async (coords) => {
   let latitudeText = language === 'ru' ? 'Широта' : 'latitude'
   let longitudeText = language === 'ru' ? 'Долгота' : 'longitude'
   const placeData = await getData(coords);
-    cityAndCountry.innerHTML = `${placeData.city?placeData.city:''} ${placeData.country}`
-    let [latitudeValue, longitudeValue] = placeData.coordinates;
-        latitude.innerHTML = `${latitudeText}: ${String(latitudeValue.toFixed(2)).replace(/[.]/g, '°')}'`;
-        longitude.innerHTML = `${longitudeText}: ${String(longitudeValue.toFixed(2)).replace(/[.]/g, '°')}'`;
-        return placeData.coordinates 
+  cityAndCountry.innerHTML = `${placeData.city?placeData.city:''} ${placeData.country}`
+  let [latitudeValue, longitudeValue] = placeData.coordinates;
+  latitude.innerHTML = `${latitudeText}: ${String(latitudeValue.toFixed(2)).replace(/[.]/g, '°')}'`;
+  longitude.innerHTML = `${longitudeText}: ${String(longitudeValue.toFixed(2)).replace(/[.]/g, '°')}'`;
+  return placeData.coordinates
 }
 
 ////////////// Обновляем время///////////////////////
@@ -207,18 +191,18 @@ const updateTime = async (coords) => {
   const coordinates = placeData.coordinates
   const timeZone = await getTimeZone(...coordinates)
   interval = setInterval(getTime.bind(null, timeZone), 1000)
-} 
+}
 
 ///////////Получаем время и выводим в приложении///////
-const getTime = (timeZone) =>{
+const getTime = (timeZone) => {
   let options = {
-    weekday : 'short',
+    weekday: 'short',
     day: 'numeric',
     month: 'long',
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric',
-    timeZone : timeZone
+    timeZone: timeZone
   }
   const date = new Date()
   datePlace.innerHTML = date.toLocaleString(`${language}`, options).replace(/,/g, '')
@@ -252,26 +236,38 @@ const getForecastWeather = async (coords) => {
   const firstDayData = forecastWeatherData[1];
   const secondDayData = forecastWeatherData[2];
   const thirdDayData = forecastWeatherData[3];
-  return {firstDayData, secondDayData, thirdDayData}
+  return {
+    firstDayData,
+    secondDayData,
+    thirdDayData
+  }
 }
 
 const updateForecastWeather = async (coords) => {
   let dayNameIndex = 0;
   let dayTempIndex = 0;
+  let dayIconIndex = 0;
   const weather = await getForecastWeather(coords)
   const arrayDayDate = [weather.firstDayData.dt * 1000, weather.secondDayData.dt * 1000, weather.thirdDayData.dt * 1000]
   const arrayDayTemp = [weather.firstDayData.temp.day.toFixed(), weather.secondDayData.temp.day.toFixed(), weather.thirdDayData.temp.day.toFixed()]
+  const arrayDayIcon = [weather.firstDayData.weather[0].icon, weather.secondDayData.weather[0].icon, weather.thirdDayData.weather[0].icon]
   forecastTempText.forEach((tempText) => {
-    if(localStorage.getItem('userTempScale') === 'F'){
+    if (localStorage.getItem('userTempScale') === 'F') {
       arrayDayTemp[dayTempIndex] = Math.round(getForengeitScale(arrayDayTemp[dayTempIndex]))
     }
     tempText.innerHTML = `${arrayDayTemp[dayTempIndex]}°`
     dayTempIndex++
   })
-    forecastDayText.forEach((dayText) => {
-      dayText.innerHTML = (new Date(arrayDayDate[dayNameIndex])).toLocaleString(`${language}`, {weekday: 'long'})
-      dayNameIndex++
-  }) 
+  forecastDayText.forEach((dayText) => {
+    dayText.innerHTML = (new Date(arrayDayDate[dayNameIndex])).toLocaleString(`${language}`, {
+      weekday: 'long'
+    })
+    dayNameIndex++
+  })
+  forecastWeatherImg.forEach((dayIcon) => {
+    dayIcon.src = `http://openweathermap.org/img/wn/${arrayDayIcon[dayIconIndex]}@4x.png`
+    dayIconIndex++
+  })
 
 }
 
@@ -285,7 +281,14 @@ const getCurrentWeather = async (coords) => {
   const wind = currentWeatherData.wind_speed
   const humidity = currentWeatherData.humidity
   const icon = currentWeatherData.weather[0].icon
-  return {description, temp, feelsLike, wind, humidity, icon}
+  return {
+    description,
+    temp,
+    feelsLike,
+    wind,
+    humidity,
+    icon
+  }
 }
 /////////////////Обновляем данные о текущей погоде///////////////
 const updateCurrentWeather = async (coords) => {
@@ -294,7 +297,7 @@ const updateCurrentWeather = async (coords) => {
   const units = language === 'ru' ? 'м/с' : 'm/s'
   const feelsLike = language === 'ru' ? 'ощущается как: ' : 'feels like: '
   const weather = await getCurrentWeather(coords)
-  if(localStorage.getItem('userTempScale') === 'F'){
+  if (localStorage.getItem('userTempScale') === 'F') {
     weather.temp = Math.round(getForengeitScale(weather.temp))
     weather.feelsLike = Math.round(getForengeitScale(weather.feelsLike))
   }
@@ -309,39 +312,56 @@ const updateCurrentWeather = async (coords) => {
 
 ///////////////////
 
+/////////////голосовой поиск///////////////////
+const startRecognizer = () => {
+  searchPlace.value = ''
+  searchPlace.classList.remove('incorrect')
+    const recognition = new webkitSpeechRecognition()
+    recognition.lang = `${language}`
 
+    recognition.onresult = (event) => {
+      const result = event.results[event.resultIndex]
+      console.clear()
+      searchPlace.value = result[0].transcript
+      searchBtn.click()
+    }
+    recognition.start();
+}
 
+microphone.addEventListener('click', startRecognizer);
+
+fBtn.addEventListener('click', () => {
+  if (!fBtnActive) return
+  fBtn.classList.add('active--temp')
+  cBtn.classList.remove('active--temp')
+  localStorage.setItem('userTempScale', 'F')
+  updateScale(getForengeitScale)
+  fBtnActive = false
+  cBtnActive = true
+})
+
+cBtn.addEventListener('click', () => {
+  if (!cBtnActive) return
+  cBtn.classList.add('active--temp')
+  fBtn.classList.remove('active--temp')
+  localStorage.setItem('userTempScale', 'C')
+  updateScale(getCelsiusScale)
+  cBtnActive = false
+  fBtnActive = true
+})
 
 searchPlace.addEventListener('focus', () => {
   searchPlace.value = '';
   searchPlaceInFocus = !searchPlaceInFocus;
+  searchPlace.classList.remove('incorrect')
 })
 searchPlace.addEventListener('blur', () => {
   searchPlaceInFocus = !searchPlaceInFocus;
 })
 
 window.addEventListener('keydown', (e) => {
-  if(e.key === 'Enter' && searchPlaceInFocus){
+  searchPlace.classList.remove('incorrect')
+  if (e.key === 'Enter' && searchPlaceInFocus) {
     searchBtn.click()
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
