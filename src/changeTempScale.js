48dd9233691b8584d9f795b219572ed7fc35f559
Pavel Dubina <Pavel_Dubina@epam.com>
2020-11-13 09:14:06 +0300

    change TempScale

--- src/changeTempScale.js
+++ src/changeTempScale.js
@@ -1,9 +1,9 @@
 import {currentWeatherTemp, feelsLikeTemp, forecastTempText} from './updateData'
 
-const cBtn = document.querySelector('.c--temperature')
-const fBtn = document.querySelector('.f--temperature')
-let cBtnActive = true
-let fBtnActive = true
+const celsiusBtn = document.querySelector('.c--temperature')
+const forengeitBtn = document.querySelector('.f--temperature')
+let celsiusBtnActive = true
+let forengeitBtnActive = true
 
 export const getForengeitScale = (celsiusTemp) => (celsiusTemp * 9 / 5) + 32
 
@@ -19,32 +19,32 @@ const updateScale = (foo) => {
 
 export const defaultOptions = () => {
     if (localStorage.getItem('userTempScale') === 'F') {
-      fBtn.classList.add('active--temp')
-      fBtnActive = false
+      forengeitBtn.classList.add('active--temp')
+      forengeitBtnActive = false
     } else {
-      cBtn.classList.add('active--temp')
-      cBtnActive = false
+      celsiusBtn.classList.add('active--temp')
+      celsiusBtnActive = false
     }
   }
 
-  fBtn.addEventListener('click', () => {
-    if (!fBtnActive) return
-    fBtn.classList.add('active--temp')
-    cBtn.classList.remove('active--temp')
+  forengeitBtn.addEventListener('click', () => {
+    if (!forengeitBtnActive) return
+    forengeitBtn.classList.add('active--temp')
+    celsiusBtn.classList.remove('active--temp')
     localStorage.setItem('userTempScale', 'F')
     updateScale(getForengeitScale)
-    fBtnActive = false
-    cBtnActive = true
+    forengeitBtnActive = false
+    celsiusBtnActive = true
   })
   
-  cBtn.addEventListener('click', () => {
-    if (!cBtnActive) return
-    cBtn.classList.add('active--temp')
-    fBtn.classList.remove('active--temp')
+  celsiusBtn.addEventListener('click', () => {
+    if (!celsiusBtnActive) return
+    celsiusBtn.classList.add('active--temp')
+    forengeitBtn.classList.remove('active--temp')
     localStorage.setItem('userTempScale', 'C')
     updateScale(getCelsiusScale)
-    cBtnActive = false
-    fBtnActive = true
+    celsiusBtnActive = false
+    forengeitBtnActive = true
   })
 
   
